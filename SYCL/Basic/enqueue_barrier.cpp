4932eda14b125e6f45f3a9089cdd2c379f5188f2
vladimirlaz <vladimir.lazarev@intel.com>
2020-10-22 16:07:44 +0300

    Disable flaky test on windows

--- SYCL/Basic/enqueue_barrier.cpp
+++ SYCL/Basic/enqueue_barrier.cpp
@@ -3,7 +3,7 @@
 // RUN: env SYCL_PI_TRACE=2 %GPU_RUN_PLACEHOLDER %t.out 2>&1 %GPU_CHECK_PLACEHOLDER
 // RUN: env SYCL_PI_TRACE=2 %ACC_RUN_PLACEHOLDER %t.out 2>&1 %ACC_CHECK_PLACEHOLDER
 
-// UNSUPPORTED: cuda
+// UNSUPPORTED: cuda || windows
 
 #include <CL/sycl.hpp>
 #include <CL/sycl/INTEL/fpga_device_selector.hpp>
